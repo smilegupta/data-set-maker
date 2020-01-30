73d2e297225ad426a7202721381565a1db8de4c4
smilegupta <guptamiley3012@gmail.com>
2020-01-30 16:28:05 +0530

    init

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+import os
+from google_images_download import google_images_download
+
+CURR_DIR = os.getcwd()
+OUTPUT_DIR = os.path.join(CURR_DIR, "output")
+
+def getDataSet(conf):
+    response = google_images_download.googleimagesdownload()
+    paths = response.download(conf)
+    return paths
+
+app=Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html',)
+
+@app.route('/getImages',  methods=['GET'])
+def get_images():
+    nclass = request.args.get('nclass')
+    nlimits = request.args.get('nlimits')
+    arguments = {"keywords": nclass,
+                "limit": nlimits,
+                "print_urls":True,
+                "output_directory": OUTPUT_DIR
+                }
+    
+    return getDataSet(arguments)
+
+
+if __name__ == '__main__':
+    app.run(host = '127.0.0.1', port = 3000)
---
+++ output/README.md
@@ -0,0 +1,1 @@
+In this directory all data images will get stored.
